e8f600e8e257f29976be12b90b06ece90c8a2da9
Evgenii Bozhkov <121394264+Jukarick@users.noreply.github.com>
2023-02-23 23:50:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,60 @@
-from module import run_game
+class Human:
+    def __init__(self, name):
+        self.name = name
 
-INTRO = '''РАССЧИТАЙ И ПОБЕДИ!
-Загрузка...
+    # ответ по умолчанию для всех одинаковый, можно 
+    # доверить его родительскому классу
+    def answer_question(self, question):
+        print('Очень интересный вопрос! Не знаю.')
 
-Твоя цель — за 5 ходов набрать такое количество очков урона противнику,
-которое попадет в диапазон +– 10 от значения здоровья противника.
 
-Значение здоровья противника генерируется случайным образом
-в диапазоне от 80 до 120 очков.
+class Student(Human):
+    #  метод ask_question() принимает параметр someone:
+    #  это объект, экземпляр класса Curator, Mentor или CodeReviewer,
+    #  которому Student задаёт вопрос; 
+    #  параметр question — это просто строка
+    #  имя объекта и текст вопроса задаются при вызове метода ask_question
+    def ask_question(self, someone, question):
+        # напечатайте на экран вопрос в нужном формате
+        print(someone, question)
+        # запросите ответ на вопрос у someone
+        print(someone.answer_question(question))
+        print()  # этот print выводит разделительную пустую строку	
 
-В твоём распоряжении три вида атак:
-lite — урон от 2 до 5 очков;
-mid — урон от 15 до 25 очков;
-hard — урон от 30 до 40 очков.
-ВПЕРЁД К ПОБЕДЕ!!!
-'''
 
-def main():
-    print(INTRO)
-    replay = True
-    while replay:
-        replay = run_game()
+class Curator(Human):
+    def answer_question(self, question):
+        if question == 'Марина, мне грустненько, что делать?':
+            print('Держись, всё получится. Хочешь видео с котиками?')
+        else:
+            super().answer_question(question)
 
-if __name__ == '__main__':
-    main()
+class CodeReviewer(Human):
+    def answer_question(self, question):
+        if question == 'Евгений, что не так с моим проектом?':
+            print('О, вопрос про проект, это я люблю.')
+        else:
+            super().answer_question(question)
+        
+class Mentor(Human):
+    def answer_question(self, question):
+        if question == 'Ира, мне грустненько, что делать?':
+            print('Отдохни и возвращайся с вопросами по теории.')
+        elif question == 'Ира, как устроиться работать питонистом?':
+            print('Сейчас расскажу.')
+        else:
+            super().answer_question(question)
+
+# следующий код менять не нужно, он работает, мы проверяли
+student1 = Student('Тимофей')
+curator = Curator('Марина')
+mentor = Mentor('Ира')
+reviewer = CodeReviewer('Евгений')
+friend = Human('Виталя')
+
+student1.ask_question(curator, 'мне грустненько, что делать?')
+student1.ask_question(mentor, 'мне грустненько, что делать?')
+student1.ask_question(reviewer, 'когда каникулы?')
+student1.ask_question(reviewer, 'что не так с моим проектом?')
+student1.ask_question(friend, 'как устроиться на работу питонистом?')
+student1.ask_question(mentor, 'как устроиться работать питонистом?')
